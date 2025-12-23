3b1d368db5dd3d0b39d3672a15e5e834f609445b
Bart de Boer <bart.de.boer@divverence.com>
2025-12-23 20:31:10 +0100

    Functionality!

--- src/Pianoteq.Pwa/Program.cs
+++ src/Pianoteq.Pwa/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using Pianoteq.Client;
 using Pianoteq.Pwa;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add Pianoteq client (configure your server URL here)
+builder.Services.AddScoped(sp => new PianoteqClient("http://192.168.86.66:8081"));
 
 await builder.Build().RunAsync();
